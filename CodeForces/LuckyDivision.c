12744237ab8e93331b7399b5a67a64612a780a9e
Esther Araújo <esthersss09@gmail.com>
2020-09-26 11:01:23 -0300

    Create LuckyDivision.c

---
+++ CodeForces/LuckyDivision.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main()
+{
+  int N, i, lucky=0, x[10]={4,7,47,74,447,474,477,774,747,744};
+	scanf("%d", &N);
+	for(i=0;i<10;i++){
+		if(N%x[i]==0){
+			lucky=1;
+			break;
+		}
+	}
+	if(lucky==1) printf("YES\n");
+	else printf("NO\n");
+	
+    return 0;
+}

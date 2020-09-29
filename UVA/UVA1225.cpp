871478b3432171f025ed934bce036efd97ed45c3
Esther Araújo <esthersss09@gmail.com>
2020-09-29 12:28:14 -0300

    Update UVA1225.cpp

--- UVA/UVA1225.cpp
+++ UVA/UVA1225.cpp
@@ -17,8 +17,10 @@ int main(){
                 x/=10;
             }
         }
-        for(int j=0;j<10;j++)
-            printf("%d ", cont[j]);
+        for(int j=0;j<10;j++){
+            printf("%d", cont[j]);
+            if(j<9) printf(" ");
+        }
         printf("\n");
 	}
     return 0;

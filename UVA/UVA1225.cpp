a6b20b1334336ea30ac0934f93fac494811ad1b5
Esther Araújo <esthersss09@gmail.com>
2020-09-29 12:16:11 -0300

    Create UVA1225.cpp

---
+++ UVA/UVA1225.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+int main(){
+    
+    int t, N, cont[10], x;
+    cin >> t;
+    while(t--){
+        cin >> N;
+        for(int k=0; k<10; k++)
+            cont[k]=0;
+
+        for(int i=1;i<=N;i++){
+            printf("Teste\n");
+            x=i;
+            while(x!=0){
+                cont[x%10]++;
+                x/=10;
+            }
+        }
+        for(int j=0;j<10;j++)
+            printf("%d ", cont[j]);
+        printf("\n");
+	}
+    return 0;
+}

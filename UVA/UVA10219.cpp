60ae1fa95d525f666ed4869fb9ebe2c5619a8ffd
Esther Araújo <esthersss09@gmail.com>
2020-10-08 13:35:46 -0300

    Create UVA10219.cpp

---
+++ UVA/UVA10219.cpp
@@ -0,0 +1,23 @@
+//Finding the Ways !
+#include <bits/stdc++.h> 
+using namespace std;
+// log(C(n, k)) = log(n!/(n - k)!k!) //Quantidade de dígitos da combinação
+//              = log(n!) - log((n - k)!k!)
+//              = log(n!) - log((n - k)!) - log(k!)
+//              = log(n!)/log(k!) - log((n - k)!)
+
+int main()
+{   
+    unsigned long long n, k, fat1, fat2;
+    while (cin >> n >> k)
+    {
+        long double sum = 0;
+        for (unsigned long long i = n - k + 1; i <= n; ++i)
+            sum += log10((double)i);
+        for (unsigned long long i = 1; i <= k; ++i)
+            sum -= log10((double)i);
+
+        cout << (int) sum + 1<< endl;
+    }
+    return 0;
+}

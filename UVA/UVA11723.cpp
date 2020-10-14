8060e805af9b4adc215666d5a33511d77647e70a
Esther Araújo <esthersss09@gmail.com>
2020-10-14 15:30:35 -0300

    Create UVA11723.cpp

---
+++ UVA/UVA11723.cpp
@@ -0,0 +1,15 @@
+//Numbering Roads
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main()
+{
+    long long road, numbers, ans, caso=1;
+    while (cin >>road>>numbers && road!=0 && numbers!=0)
+    {
+        ans=(road-1)/numbers;
+        if(ans>26) cout << "Case " <<caso<<": Impossible"<<endl;
+        else cout << "Case " <<caso<<": "<<ans<<endl;
+        caso++;
+    }
+}

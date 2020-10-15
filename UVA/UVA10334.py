9120e0cf309be6eb4cd23cfadfbd5afa2f1655ec
Esther Araújo <esthersss09@gmail.com>
2020-10-15 20:06:56 -0300

    Create UVA10334.py

---
+++ UVA/UVA10334.py
@@ -0,0 +1,10 @@
+//Ray Through Glasses
+F=[1,2,3]
+for i in range(3, 1001):
+    F.append(F[i-1]+F[i-2])
+while(1):
+    try:
+        n = int(input())
+        print(F[n])
+    except EOFError:
+        break

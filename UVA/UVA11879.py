bd1df9fa4e14fc4ffa8585fc3332badb1df95b4f
Esther Araújo <esthersss09@gmail.com>
2020-10-15 20:46:36 -0300

    Create UVA11879.py

---
+++ UVA/UVA11879.py
@@ -0,0 +1,13 @@
+#Multiple of 17
+while True:
+    n = int(input())
+    if n == 0:
+        break
+   
+    while n > 100:
+        n = n // 10 - 5 * (n % 10)
+
+    if not n % 17:
+        print("1")
+    else:
+        print("0")

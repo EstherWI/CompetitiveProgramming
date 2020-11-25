aa8b5922f48465baf7b84572080f47dbf5c7d56e
Esther Araújo <esthersss09@gmail.com>
2020-11-24 22:59:40 -0300

    Create SubtractOrDivide.cpp

---
+++ CodeForces/SubtractOrDivide.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, n;
+    cin >> t;
+    while(t--) {
+        cin >> n;
+        cout << (n <= 3? n-1 : 2 + (n&1)) << '\n';
+    }
+    return 0;
+}

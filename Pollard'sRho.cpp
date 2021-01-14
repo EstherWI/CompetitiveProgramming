b6fb09dfef8d92a2385a3e722cb7b03d1bec07b3
Esther Araújo <esthersss09@gmail.com>
2021-01-14 15:02:51 -0300

    Update Pollard'sRho.cpp

--- Pollard'sRho.cpp
+++ Pollard'sRho.cpp
@@ -2,69 +2,35 @@ Pollard's Rho algorithm */
 #include<bits/stdc++.h> 
 using namespace std; 
 
-/* Function to calculate (base^exponent)%modulus */
-long long int modular_pow(long long int base, int exponent, long long int modulus) 
-{ 
-	/* initialize result */
-	long long int result = 1; 
+#define abs_val(a) (((a)>0)?(a):-(a))
+typedef long long ll;
+ll mulmod(ll a, ll b, ll c) { // returns (a * b) % c, and minimize overflow
+	ll x = 0, y = a % c;
+	while (b > 0) {
+		if (b % 2 == 1) x = (x + y) % c;
+		y = (y * 2) % c;
+		b /= 2;
+	}
+	return x % c;
+}
 
-	while (exponent > 0) 
-	{ 
-		/* if y is odd, multiply base with result */
-		if (exponent & 1) 
-			result = (result * base) % modulus; 
+ll gcd(ll a,ll b) { return !b ? a : gcd(b, a % b); } // standard gcd
 
-		/* exponent = exponent/2 */
-		exponent = exponent >> 1; 
+ll pollard_rho(ll n) {
+	int i = 0, k = 2;
+	ll x = 3, y = 3; // random seed = 3, other values possible
+	while (1) {
+		i++;
+		x = (mulmod(x, x, n) + n - 1) % n; // generating function
+		ll d = gcd(abs_val(y - x), n); // the key insight
+		if (d != 1 && d != n) return d; // found one non-trivial factor
+		if (i == k) y = x, k *= 2;
+	}
+}
 
-		/* base = base * base */
-		base = (base * base) % modulus; 
-	} 
-	return result; 
-} 
-
-/* method to return prime divisor for n */
-long long int PollardRho(long long int n) 
-{ 
-	/* initialize random seed */
-	srand (time(NULL)); 
-
-	/* no prime divisor for 1 */
-	if (n==1) return n; 
-
-	/* even number means one of the divisors is 2 */
-	if (n % 2 == 0) return 2; 
-
-	/* we will pick from the range [2, N) */
-	long long int x = (rand()%(n-2))+2; 
-	long long int y = x; 
-
-	/* the constant in f(x). 
-	* Algorithm can be re-run with a different c 
-	* if it throws failure for a composite. */
-	long long int c = (rand()%(n-1))+1; 
-
-	/* Initialize candidate divisor (or result) */
-	long long int d = 1; 
-
-	/* until the prime factor isn't obtained. 
-	If n is prime, return n */
-	while (d==1) 
-	{ 
-		/* Tortoise Move: x(i+1) = f(x(i)) */
-		x = (modular_pow(x, 2, n) + c + n)%n; 
-
-		/* Hare Move: y(i+1) = f(f(y(i))) */
-		y = (modular_pow(y, 2, n) + c + n)%n; 
-		y = (modular_pow(y, 2, n) + c + n)%n; 
-
-		/* check gcd of |x-y| and n */
-		d = __gcd(abs(x-y), n); 
-
-		/* retry if the algorithm fails to find prime factor 
-		* with chosen x and c */
-		if (d==n) return PollardRho(n); 
-	} 
-
-	return d; 
-} 
+int main() {
+	ll n = 2063512844981574047LL; // we assume that n is not a large prime
+	ll ans = pollard_rho(n); // break n into two non trivial factors
+	if (ans > n / ans) ans = n / ans; // make ans the smaller factor
+	printf("%lld %lld\n", ans, n / ans); // should be: 1112041493 1855607779
+} // return 0;

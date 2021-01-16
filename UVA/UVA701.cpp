7e9cc6233dd3d7d5b1e54927bf6d4d5b22e5427a
Esther Araújo <esthersss09@gmail.com>
2021-01-15 22:17:04 -0300

    Create UVA701.cpp

---
+++ UVA/UVA701.cpp
@@ -0,0 +1,35 @@
+//The Archeologists' Dilemma
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main  () {
+	int x, y;
+	double l, r, d, a, b;
+	while (cin>>x) {
+		y = x;
+		int c = 0;
+		while (y) {
+			c++;
+			y /= 10;
+		}
+ 
+		l = log(x) / log(2);
+		r = log(x + 1) / log(2);
+		d = log(10) / log(2);
+ 
+		for (int i = c + 1; ; i++) {
+			a = l + i * d;
+			b = r + i * d;
+ 
+			if (ceil(a) <= floor(b)) {
+				printf("%.0lf\n", ceil(a));
+				break;
+			} else if (i == 1000000) {
+					printf("no power of 2\n");
+					break;
+			}
+		}
+	}
+	return 0;
+}

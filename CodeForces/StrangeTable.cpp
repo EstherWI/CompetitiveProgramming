b0964804e3cdfb96f8ae84ec9867669f5c789c6b
Esther Araújo <esthersss09@gmail.com>
2021-04-10 21:43:14 -0300

    Update StrangeTable.cpp

--- CodeForces/StrangeTable.cpp
+++ CodeForces/StrangeTable.cpp
@@ -6,12 +6,12 @@ int main  () {
  
   long long int t, n, m, x, resp, linha, coluna, o;
   cin>>t;
-	while (t--) {
-		cin>>n>>m>>x;
-    x=x-1;
-    linha=x/n;
-    coluna=x%n;
-    cout<<coluna*m + linha + 1<<endl;
-	}
-	return 0;
+  while (t--) {
+	  cin>>n>>m>>x;
+	  x=x-1;
+	  linha=x/n;
+	  coluna=x%n;
+	  cout<<coluna*m + linha + 1<<endl;
+ }
+  return 0;
 }

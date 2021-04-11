c5fedfd9b4131544cf89256d847ac95db02956fa
Esther Araújo <esthersss09@gmail.com>
2021-04-10 21:42:10 -0300

    Create StrangeTable.cpp

---
+++ CodeForces/StrangeTable.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main  () {
+ 
+  long long int t, n, m, x, resp, linha, coluna, o;
+  cin>>t;
+	while (t--) {
+		cin>>n>>m>>x;
+    x=x-1;
+    linha=x/n;
+    coluna=x%n;
+    cout<<coluna*m + linha + 1<<endl;
+	}
+	return 0;
+}
